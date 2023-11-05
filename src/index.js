2f35569ef75510ec6576000610f2ef315d2c0145
ElPokaReal <therealpoka211@gmail.com>
2023-11-05 13:34:44 -0400

    Primeros cambios

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+import { port } from "./config.js";
+
+app.use(cors());
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get("/", (req, res) => {
+    res.json({ message: "API Oficial de Agrotrujillo"});
+});
+
+app.use(router);
+
+app.use((err, req, res, next) => {
+    return res.status(500).json({
+        status: "error",
+        message: err.message,
+    });
+});
+
+app.listen(port)
+console.log(`Servidor corriendo en el puerto: ${PORT}`);
